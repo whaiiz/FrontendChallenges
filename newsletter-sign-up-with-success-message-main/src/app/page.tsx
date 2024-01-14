e09216a88d7a86c322b2ee1b415dc15e3b19af8e
Jaime <jaime_reis18@outlook.pt>
2024-01-14 18:01:19 -0100

    Subscribe page mobile

--- newsletter-sign-up-with-success-message-main/src/app/page.tsx
+++ newsletter-sign-up-with-success-message-main/src/app/page.tsx
@@ -1,24 +1,24 @@
-import Image from 'next/image'
 import styles from './page.module.css'
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <article>
-      <section>
-        <h1>Stay Updated</h1>
-        <p>Join 60,000 + products managers receiving monthly updates on:</p>
+    <article className={styles.subscribeNewsletter}>
+      <img className={styles.desktopImage} src="./illustration-sign-up-desktop.svg" />
+      <img className={styles.mobileImage} src="./illustration-sign-up-mobile.svg" />
+      <section className={styles.subscribeNewsletterFormContainer}>
+        <h1>Stay updated!</h1>
+        <p className={styles.subscribeNewsletterText}>Join 60,000 + products managers receiving monthly updates on:</p>
         <ul>
           <li>Product discovery and building what matters</li>
           <li>Measuring to ensure updates are success</li>
           <li>And much more!</li>
         </ul>
-        <label>Email address</label>
-        <input type="text" placeholder="email@company.com" />
-        <button>Subscribe to monthly newsletter</button>
+        <form>          
+          <label>Email address</label>
+          <input type="text" placeholder="email@company.com" />
+          <button>Subscribe to monthly newsletter</button>
+        </form>
       </section>
-      <img src="./illustration-sign-up-desktop.svg" />
     </article>
-    </main>
   )
 }
